976f0f889e70af20c5d94a9275e533e261bd8bdd
Hugo Janssen <janssenlive@gmail.com>
2025-03-14 12:53:42 -0600

    Added UDP classes for send and receive messages

---
+++ Assets/Scripts/UDP/UDPSender.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UDPSender
+{
+    private UdpClient udpSender;
+    private IPEndPoint remoteEndPoint;
+
+    public UDPSender(string ipAddress, int port)
+    {
+        udpSender = new UdpClient();
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+    }
+
+    public void SendMessage(string message)
+    {
+        byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+        udpSender.Send(messageBytes, messageBytes.Length, remoteEndPoint);
+        Debug.Log("Message sent: " + message);
+    }
+
+    public void Close()
+    {
+        udpSender.Close();
+    }
+}
