1ea602675670aa148ae2e979f65cb833b58d7be8
Hugo Janssen <janssenlive@gmail.com>
2025-03-26 12:30:01 -0600

    Added field prefabs to networkmanagerui

--- Assets/Scripts/UI/NetworkManagerUI.cs
+++ Assets/Scripts/UI/NetworkManagerUI.cs
@@ -8,27 +8,27 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
-    [SerializeField] private InputField ipLabel;
-    [SerializeField] private InputField portLabel;
+    [SerializeField] private TMPro.TMP_InputField ipField;
+    [SerializeField] private TMPro.TMP_InputField portField;
 
     private void Awake()
     {
         serverBtn.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipLabel.text, 
-                (ushort)int.Parse(portLabel.text)); 
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipField.text, 
+                (ushort)int.Parse(portField.text)); 
             NetworkManager.Singleton.StartServer();
         });
         hostBtn.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipLabel.text,
-                (ushort)int.Parse(portLabel.text));
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipField.text,
+                (ushort)int.Parse(portField.text));
             NetworkManager.Singleton.StartHost();
         });
         clientBtn.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipLabel.text,
-                (ushort)int.Parse(portLabel.text));
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipField.text,
+                (ushort)int.Parse(portField.text));
             NetworkManager.Singleton.StartClient();
         });
     }
