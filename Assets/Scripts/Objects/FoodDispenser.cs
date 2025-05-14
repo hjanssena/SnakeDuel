cfcd4cd0e30e698548f83b0fde0d866f6130265f
hjanssen <janssenlive@gmail.com>
2025-05-14 17:23:05 -0600

    Things

--- Assets/Scripts/Objects/FoodDispenser.cs
+++ Assets/Scripts/Objects/FoodDispenser.cs
@@ -26,8 +26,33 @@ public class FoodDispenser : NetworkBehaviour
 
     void SpawnFood()
     {
+        Vector2 spawnPosition;
+        do
+        {
+            spawnPosition = new Vector2(Random.Range(-12, 4), Random.Range(-8, 8));
+        } while (!checkIfFreePosition(spawnPosition)) ;
         GameObject newFood = Instantiate(food, new Vector2(Random.Range(-12, 4), Random.Range(-8, 8)), Quaternion.identity);
         NetworkObject netFood = newFood.GetComponent<NetworkObject>();
         netFood.Spawn();
+    }
+
+    bool checkIfFreePosition(Vector2 position)
+    {
+        LayerMask mask = LayerMask.GetMask("Snake") | LayerMask.GetMask("Wall");
+        RaycastHit2D hitU = Physics2D.Raycast(position, Vector2.up, .48f, mask);
+        RaycastHit2D hitD = Physics2D.Raycast(position, Vector2.down, .48f, mask);
+        RaycastHit2D hitL = Physics2D.Raycast(position, Vector2.left, .48f, mask);
+        RaycastHit2D hitR = Physics2D.Raycast(position, Vector2.right, .48f, mask);
+
+        bool result;
+        if (!hitU && !hitD && !hitL && !hitR)
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        return result;
     }
 }
